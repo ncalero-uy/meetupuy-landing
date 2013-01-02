6a5a909ea1324db91ffac7122059b08ff30168ce
Diego Sapriza <AV4TAr@gmail.com>
2013-01-02 15:34:07 -0200

    cleanup

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 require_once 'include/eventos.inc.php';
-$anuales_chunks = array_chunk($anuales, 2);
-$mensuales_chunks = array_chunk($mensuales, 2);
 
 function print_bloque($evento) {
     ?>
     <div class="span5 well2">
         <h2 class="sub"><a href="<?= $evento['link']; ?>" target="_blank"><?= $evento['titulo']; ?></a></h2>
-        <h4><?php if(isset($evento['subtitulo'])) { echo $evento['subtitulo']; } ?></h4>
+        <h4><?php if (isset($evento['subtitulo'])) {
+        echo $evento['subtitulo'];
+    } ?></h4>
         <ul class="unstyled">
             <li><i class="icon-map-marker"></i> <?= $evento['lugar']; ?></li>
             <?php if (!empty($evento['fecha']) && $evento['fecha'] != ''): ?>
-            <li><i class="icon-calendar"></i> <?= $evento['fecha']; ?></li>
+                <li><i class="icon-calendar"></i> <?= $evento['fecha']; ?></li>
             <?php endif; ?>
             <?php if (!empty($evento['horario']) && $evento['horario'] != ''): ?>
                 <li><i class="icon-time"></i> <?= $evento['horario']; ?></li>
@@ -22,55 +22,36 @@ function print_bloque($evento) {
             <?php endif; ?>
 
             <?php
-                if (!empty($evento['links_otros']) && is_array($evento['links_otros']) && count($evento['links_otros'])> 0){
-                    echo '<li class="otros-links"><i class="icon-share"></i> ';
-                    foreach($evento['links_otros'] as $link_otro){
-                        echo '<span><a rel="tooltip" href="'.$link_otro['uri'].'" target="_blank" alt="'.$link_otro['alt'].'" title="'.$link_otro['alt'].'">'.$link_otro['txt'].'</a></span>'
-                        ?>
+            if (!empty($evento['links_otros']) && is_array($evento['links_otros']) && count($evento['links_otros']) > 0) {
+                echo '<li class="otros-links"><i class="icon-share"></i> ';
+                foreach ($evento['links_otros'] as $link_otro) {
+                    echo '<span><a rel="tooltip" href="' . $link_otro['uri'] . '" target="_blank" alt="' . $link_otro['alt'] . '" title="' . $link_otro['alt'] . '">' . $link_otro['txt'] . '</a></span>'
+                    ?>
 
-                        <?php
-                    }
-                    echo '</li>';
-             }
-             ?>
+                    <?php
+                }
+                echo '</li>';
+            }
+            ?>
 
             <?php
-                if (!empty($evento['tags']) && $evento['tags'] != ''){
-                    echo '<li><i class="icon-tag"></i> ';
-                    $tags_array = explode(',',$evento['tags']);
-                    foreach($tags_array as $tag){
-                        $tag = trim($tag);
-                        echo '<span class="label label-info">'.$tag.'</span>'
-                        ?>
+            if (!empty($evento['tags']) && $evento['tags'] != '') {
+                echo '<li><i class="icon-tag"></i> ';
+                $tags_array = explode(',', $evento['tags']);
+                foreach ($tags_array as $tag) {
+                    $tag = trim($tag);
+                    echo '<span class="label label-info">' . $tag . '</span>'
+                    ?>
 
-                        <?php
-                    }
-                    echo '</li>';
-             }
-             ?>
+                    <?php
+                }
+                echo '</li>';
+            }
+            ?>
         </ul>
     </div>
     <?php
 }
-
-
-$imagenes_ediciones_anteriores = array();
-//tech.meetup.uy
-for ($i = 1; $i <= 14; $i++) {
-    $imagenes_ediciones_anteriores[] = array(
-            'link_site' => 'http://tech.meetup.uy',
-            'link_imagen' => 'http://tech.meetup.uy/img/2012_presentacion/tm2012-' . sprintf('%02d', $i) . '.jpg',
-        );
-}
-//devfest
-for ($i = 1; $i <= 10; $i++) {
-    $imagenes_ediciones_anteriores[] = array(
-        'link_site' => 'http://gdg.uy/',
-        'link_imagen' => '/img/2012/devfest/' . sprintf('%02d', $i) . '.jpg',
-    );
-}
-
-shuffle($imagenes_ediciones_anteriores);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -130,46 +111,6 @@ shuffle($imagenes_ediciones_anteriores);
                 </div>
             </div>
         </div>
-        <!--
-        <div id="landing">
-            <div class="container">
-
-                <div class="row">
-                    <div class="span8 offset2 txt-middle">
-                        <div id="myCarousel" class="carousel slide">
-                            <h3>Ediciones anteriores:</h3>
-                            <div class="carousel-inner">
-                                <?php
-                                $i = 1;
-                                foreach($imagenes_ediciones_anteriores as $imagenes){
-                                        $active = '';
-                                        if ($i == 1) {
-                                            $active = 'active';
-                                            $i = 2;
-                                        }
-                                        echo '<div class="' . $active . ' item"><a href="'.$imagenes['link_site'].'" target="_blank"><img alt="" src="'.$imagenes['link_imagen'].'"></a></div>';
-                                }
-                                /*
-                                for ($i = 1; $i <= 14; $i++) {
-                                    $active = '';
-                                    if ($i == 1) {
-                                        $active = 'active';
-                                    }
-                                    echo '<div class="' . $active . ' item"><a href="http://tech.meetup.uy" target="_blank"><img alt="" src="http://tech.meetup.uy/img/2012_presentacion/tm2012-' . sprintf('%02d', $i) . '.jpg"></a></div>';
-                                }
-                                 *
-                                 */
-                                ?>
-                            </div>
-                            <a class="carousel-control left" href="#myCarousel" data-slide="prev"><i class="icon-chevron-left"></i></a>
-                            <a class="carousel-control right" href="#myCarousel" data-slide="next"><i class="icon-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        -->
         <div id="content">
             <div class="container">
                 <div class="row">
@@ -178,19 +119,20 @@ shuffle($imagenes_ediciones_anteriores);
                         <hr class="dashed">
                     </div>
                 </div>
-<?php
-foreach ($anuales_chunks as $eventos_row):
-    ?>
+                <?php
+                $anuales_chunks = array_chunk($anuales, 2);
+                foreach ($anuales_chunks as $eventos_row):
+                    ?>
                     <div class="row">
-                    <?php
-                    foreach ($eventos_row as $evento):
-                        print_bloque($evento);
-                    endforeach;
-                    ?>
-                    </div>
                         <?php
-                    endforeach;
-                    ?>
+                        foreach ($eventos_row as $evento):
+                            print_bloque($evento);
+                        endforeach;
+                        ?>
+                    </div>
+                    <?php
+                endforeach;
+                ?>
 
                 <div class="row">
                     <div class="span12">
@@ -198,19 +140,20 @@ foreach ($anuales_chunks as $eventos_row):
                         <hr class="dashed">
                     </div>
                 </div>
-<?php
-foreach ($mensuales_chunks as $eventos_row):
-    ?>
+                <?php
+                $mensuales_chunks = array_chunk($mensuales, 2);
+                foreach ($mensuales_chunks as $eventos_row):
+                    ?>
                     <div class="row">
-                    <?php
-                    foreach ($eventos_row as $evento):
-                        print_bloque($evento);
-                    endforeach;
-                    ?>
-                    </div>
                         <?php
-                    endforeach;
-                    ?>
+                        foreach ($eventos_row as $evento):
+                            print_bloque($evento);
+                        endforeach;
+                        ?>
+                    </div>
+                    <?php
+                endforeach;
+                ?>
                 <!--
                 <div class="row">
                     <div class="span12">
@@ -231,52 +174,11 @@ foreach ($mensuales_chunks as $eventos_row):
                 </div>
             </div>
         </div>
-        <!--
-        <div id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="span3">
-                        <h3>Quick Links</h3>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">Pricing</a></li>
-                        </ul>
-                    </div>
-                    <div class="span3">
-                        <h3>Company</h3>
-                        <ul>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">FAQ</a></li>
-                        </ul>
-                    </div>
-                    <div class="span3">
-                        <h3>We're Social</h3>
-                        <ul>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Twitter</a></li>
-                            <li><a href="#">Google &#43;</a></li>
-                        </ul>
-                    </div>
-                    <div class="span3">
-                        <h3>Subscribe</h3>
-                        <p>Subscribe to our newsletter and stay up to date with the latest news and deals!</p>
-                        <form>
-                            <input class="span3" type="text" placeholder="Your Email" name="subscribe">
-                            <button class="btn btn-primary">Subscribe</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        -->
         <div id="copywrite">
             <div class="container">
                 <div class="row">
                     <div class="span12">
-                        <p>&copy; 2012 tech.Meetup.UY <span id="totop" class="pull-right">Back to Top <i class="icon-arrow-up"></i></span></p>
+                        <p>&copy; 2013 Meetup.UY <span id="totop" class="pull-right">Back to Top <i class="icon-arrow-up"></i></span></p>
                     </div>
                 </div>
             </div>
